0a4450b73922d58a947d61f44ba7a28ebf6d8806
Lauren DeLong <l.n.delong@sms.ed.ac.uk>
2022-08-08 12:33:05 +0100

    Bug fix for contingency table funct

--- clustr/utils.py
+++ clustr/utils.py
@@ -119,10 +119,10 @@ def plot_freqs(df: pd.DataFrame,
     if pvalue_dict:
         freqs = freqs[[cond for cond, pval in pvalue_dict[cluster_no].items() if pval < 0.05]]
         adj = adj[[cond for cond, pval in pvalue_dict[cluster_no].items()if pval < 0.05]]
-    freqs.plot.bar()
-    plt.yticks(rotation=90)
-    plt.ylabel("Frequency")
-    plt.savefig(osp.join(out_folder, "frequencies.png"), dpi=300, bbox_inches='tight')
+    #freqs.plot.bar()
+    #plt.yticks(rotation=90)
+    #plt.ylabel("Frequency")
+    #plt.savefig(osp.join(out_folder, f"cluster_{cluster_no}_frequencies.png"), dpi=300, bbox_inches='tight')
     adj.plot.bar()
     plt.yticks(rotation=90)
     plt.ylabel("Adjusted Relative Frequency")
