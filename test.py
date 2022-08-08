4279547c6bc81b18d4714a70924e004e9ad8090d
Lauren DeLong <l.n.delong@sms.ed.ac.uk>
2022-08-08 08:36:36 +0100

    Adding test to check memory consumption

---
+++ test.py
@@ -0,0 +1,47 @@
+import click
+import logging
+import os.path as osp
+from memory_profiler import profile
+import pandas as pd
+from clustr.constants import PROCESSED_DATA, HIER_AGG_RESULTS, LCA_RESULTS, KMEDOIDS_RESULTS, KMODES_RESULTS
+from clustr.utils import get_data, plot_morbidity_dist
+from clustr.hier_agg_utils import get_agg_clusters, plot_dendrogram
+
+
+@profile
+def test_agg1():
+    """TODO"""
+    df, mat, pat_ids, labs, cgrps = get_data(osp.join(PROCESSED_DATA, 'gp_mmorbs.tsv'), 0.01, True)
+    model, labels = get_agg_clusters(mat, 'hamming', 'complete')
+    plot_dendrogram(mat, 'dendrogram.png', 'hamming', 'complete')
+    df['aggl_cluster_labels'] = labels
+    df.to_csv(osp.join(HIER_AGG_RESULTS, 'gp_labs.tsv'), sep='\t')
+    plot_morbidity_dist(df, 'aggl_cluster_labels', HIER_AGG_RESULTS, 'agglomerative_hierarchical')
+
+
+@profile
+def test_agg2():
+    """TODO"""
+    df, mat, pat_ids, labs, cgrps = get_data(osp.join(PROCESSED_DATA, 'gp_mmorbs.tsv'), 0.05, True)
+    model, labels = get_agg_clusters(mat, 'hamming', 'complete')
+    plot_dendrogram(mat, 'dendrogram.png', 'hamming', 'complete')
+    df['aggl_cluster_labels'] = labels
+    df.to_csv(osp.join(HIER_AGG_RESULTS, 'gp_labs.tsv'), sep='\t')
+    plot_morbidity_dist(df, 'aggl_cluster_labels', HIER_AGG_RESULTS, 'agglomerative_hierarchical')
+
+
+@profile
+def test_agg3():
+    """TODO"""
+    df, mat, pat_ids, labs, cgrps = get_data(osp.join(PROCESSED_DATA, 'gp_mmorbs.tsv'), 0.1, True)
+    model, labels = get_agg_clusters(mat, 'hamming', 'complete')
+    plot_dendrogram(mat, 'dendrogram.png', 'hamming', 'complete')
+    df['aggl_cluster_labels'] = labels
+    df.to_csv(osp.join(HIER_AGG_RESULTS, 'gp_labs.tsv'), sep='\t')
+    plot_morbidity_dist(df, 'aggl_cluster_labels', HIER_AGG_RESULTS, 'agglomerative_hierarchical')
+
+
+if __name__ == '__main__':
+    test_agg1()
+    test_agg2()
+    test_agg3()
