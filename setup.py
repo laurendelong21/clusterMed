089b0fe7b76c404cc3cd026fb40e29ed065d8c58
Lauren DeLong <l.n.delong@sms.ed.ac.uk>
2024-04-08 15:50:38 +0100

    Adding ipykernel

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 
 requirements = ['bayespy==0.5.22',
                 'click==8.1.3',
+                'ipykernel==6.7.0',
                 'kmodes==0.12.1',
                 'matplotlib==3.5.2',
                 'numpy==1.22.3',
@@ -23,14 +24,14 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Lauren Nicole DeLong",
     author_email='l.n.delong@sms.ed.ac.uk',
-    python_requires='>=3.9',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.7'
     ],
     description="UK BioBank Multimorbidity Clustering",
     entry_points={
